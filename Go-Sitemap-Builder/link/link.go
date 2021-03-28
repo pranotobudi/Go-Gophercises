2cddb178e88480025f506853bcded0f858a2e8ea
Pranoto Budi <pranotobudi.s@gmail.com>
2021-03-28 09:57:12 +0700

    initiate Sitemap Builder project

---
+++ Go-Sitemap-Builder/link/link.go
@@ -0,0 +1,92 @@
+package link
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+//Link represents a link (<a href="">) in html document
+type Link struct {
+	Href string
+	Text string
+}
+
+// var r io.Reader
+
+// links, err := link.Parse(r)
+
+//Parse will take in an HTML document and will return a slice of links parsed from it
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	nodes := LinkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, BuildLink(node))
+		fmt.Printf("%v \n", node)
+	}
+	// dfs(doc, "")
+
+	return links, nil
+}
+
+func BuildLink(n *html.Node) Link {
+	var result Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			result.Href = attr.Val
+			break
+		}
+	}
+	result.Text = BuildText(n)
+
+	return result
+}
+
+func BuildText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var result string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result += BuildText(c) + " "
+	}
+
+	temp_result_slice := strings.Fields(result) //trim all whitespace and return slice of word in strings
+	result = strings.Join(temp_result_slice, " ")
+	return result
+}
+
+func LinkNodes(n *html.Node) []*html.Node {
+	var result []*html.Node
+	if n.Type == html.ElementNode && n.Data == "a" {
+		//this block is the base condition
+		return append(result, n)
+		// return []*html.Node{n}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result = append(result, LinkNodes(c)...)
+	}
+	return result
+
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
+}
